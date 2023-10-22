7caa0f5d7273bf40863f58a173a0a4319801273f
MaggieMeow <maggieliyd@gmail.com>
2023-10-22 18:56:57 +1100

    update image links

--- src/data/recipes.ts
+++ src/data/recipes.ts
@@ -3,9 +3,9 @@ export const recipes = [
     title: "dahl",
     coverImageUrl: "public/illustrations/dahl.png",
     screenshotUrls: [
-      "public/screenshots/dahl_01.png",
-      "public/screenshots/dahl_02.png",
-      "public/screenshots/dahl_03.png",
+      "screenshots/dahl_01.png",
+      "screenshots/dahl_02.png",
+      "screenshots/dahl_03.png",
     ],
     content:
       "The celebrated Indian and Chinese dish called dahl has also lentils for its chief ingredient and is purely of vegetable matter, it is substantial and delicials, and is made as follows:-Stew a quart of split lentils till they form a thick soup; have ready a pound of rice, well boiled in milk, and drained off as dry as possible. Shake the rice up loosely in a dish, and, after mixing an ounce of currypowder with the lentils, pour the lentil soup over the rice and serve it up.",
@@ -16,8 +16,8 @@ export const recipes = [
   },
   {
     title: "melon jam",
-    coverImageUrl: "public/illustrations/melon jam.png",
-    screenshotUrls: ["public/screenshots/melon jam.png"],
+    coverImageUrl: "illustrations/melon jam.png",
+    screenshotUrls: ["screenshots/melon jam.png"],
     content:
       "The quantity is for 20lb., and it is made as follows:- 20lb. of melon cut into large pieces, 20lb. of sugar, the rind and juice of four lemons, 2oz. of green ginger boiled and sliced, and one teaspoonful of cayenne pepper tied tightly in a thick piece of muslin. Boil all together till the melon is clear ; when cooked take out the pepper, add one glass of brandy. N. B -Green ginger can be bought from the Chinese or from Mr. Germain Nicoleon, grocer.",
     date: "Saturday 14 April 1883",
@@ -42,8 +42,8 @@ export const recipes = [
   },
   {
     title: "preserved ginger",
-    coverImageUrl: "public/illustrations/preserved-ginger.webp",
-    screenshotUrls: ["public/screenshots/preserved-ginger-screenshot.jpeg"],
+    coverImageUrl: "illustrations/preserved-ginger.webp",
+    screenshotUrls: ["screenshots/preserved-ginger-screenshot.jpeg"],
     content: `After many inquiries I am unable to tell you where green ginger roots can be bought,
       but I can give you an excellent substitute if you wish to
       preserve ginger for your own use. Take the common dry
@@ -66,12 +66,12 @@ export const recipes = [
   },
   {
     title: "gingembre a la chantilly",
-    coverImageUrl: "public/illustrations/gingembre a la chantilly.png",
+    coverImageUrl: "illustrations/gingembre a la chantilly.png",
     screenshotUrls: [
-      "public/screenshots/Gingembre a la Chantilly_01.png",
-      "public/screenshots/Gingembre a la Chantilly_02.png",
-      "public/screenshots/Gingembre a la Chantilly_03.png",
-      "public/screenshots/Gingembre a la Chantilly_04.png",
+      "screenshots/Gingembre a la Chantilly_01.png",
+      "screenshots/Gingembre a la Chantilly_02.png",
+      "screenshots/Gingembre a la Chantilly_03.png",
+      "screenshots/Gingembre a la Chantilly_04.png",
     ],
     content:
       "Gingembre a la Chantilly. —Take from 4 to 8 oz. of. Chinese preserved ginger. Cut it quickly into neat squares; Pile those up.: in the centre of a fancy dish. If time permits, whip, up a gill of cream (the contents of one of the sixpenny brown pots gives about this quantity), and pile in on top of the ginger. If pressed for time, pour the cream over it. It does not require sweetening, as the ginger is in itself excessively sweet. Then take a gill of the ginger syrup, add to it tablespoonful of either brandy, rum, or whisky, as preferred. Pour over the cream, and serve.",
@@ -80,10 +80,36 @@ export const recipes = [
     publisher: "Darling Downs Gazette (Qld. : 1881 - 1922)",
     tags: ["fusion", "dessert"],
   },
+
+  {
+    title: "chop suey",
+    coverImageUrl: "illustrations/chop-suey.webp",
+    screenshotUrls: ["screenshots/chop-suey.png"],
+    date: "17 April 1928",
+    content: `Chop Suey (Chinese Rice)  
+      One pound fresh pork cut very thin, 2  
+      onions thinly sliced, 1 cup chopped celery,  
+      1 cup mushrooms, 3 tablespoons soya-bean  
+      sauce, 1 cup water or stock. Fry meat 5  
+      
+      minutes without browning, fry onions, add  
+      celery, fry 5 minutes, put in meat, sauce  
+      and stock; cook gently 25 minutes. Serve  
+      with rice. To Prepare the Rice: Put 1 cup  
+      rice in 2 cups cold water, bring to boil and  
+      boil for 10 minutes (scrape from bottom  
+      of saucepan at first, as rice always sticks) ;  
+      then put on corner of stove to finish cooking without boiling. All water will be  
+      absorbed and each grain will be separate.  
+      —Nancy Dere, Hawera, N.Z. `,
+    troveUrl: "https://nla.gov.au/nla.obj-416873958",
+    publisher: "The Australian woman's mirror",
+    tags: ["dish"],
+  },
   {
     title: "dim sims",
-    coverImageUrl: "public/illustrations/dim sims.png",
-    screenshotUrls: ["public/screenshots/dim sims.png"],
+    coverImageUrl: "illustrations/dim sims.png",
+    screenshotUrls: ["screenshots/dim sims.png"],
     content:
       'Dim Sims Mr H (Melbourne) supplies this recipe for making dim sims: -Chicken pork or a mixture of both, may be minced or finely shopped to form the base of the filling. Blend with either finely chopped water chestnuts obtainable from a Chinese restaurant and a little "Seeyou" sauce (or Worcester sauce may be used). Add pepper and salt to taste, and fill with the mixture small cases made from leaf macaroni. Ordinary boiling paste may be used if the macaroni is unobtainable Stand in a saucepan containing sufficient boiling water to cover them and simmer gently. Other seasonings may be tested as the art of making this Chinese dish is mastered.',
     date: "Wednesday 15 May 1935",
@@ -92,13 +118,24 @@ export const recipes = [
     tags: ["dish"],
   },
   {
+    title: "chinese chews",
+    coverImageUrl: "public/illustrations/chinese chews dessert.png",
+    screenshotUrls: ["public/screenshots/chinese chews.png"],
+    date: "4 June 1935",
+    content: `One cup sugar, 31b. butter, 1 egg, 1 cup flour, 3 teaspoons cocoa, vanilla to taste, 1 cup chopped dates or walnuts, or both. Cream butter and sugar, add egg and beat again. Then add dry ingredients and fruit. This is a thick enough mixture to spread over greased meat-tin or long sponge-roll tin. Take out of oven before it looks  thoroughly cooked (about 3/4 hour) ; this is important, for if it is well cooked it will be dry instead of chewy. Cut into squares in tin while hot then leave to cool.— \"Ngawini,\” Wanganui, N.Z.  ",`,
+    troveUrl: "https://nla.gov.au/nla.obj-566554307",
+    publisher: "The Australian woman's mirror.",
+    tags: ["dessert"],
+  },
+
+  {
     title: "mustard pickle",
-    coverImageUrl: "public/illustrations/chinese mustard pickle.png",
+    coverImageUrl: "illustrations/chinese mustard pickle.png",
     screenshotUrls: [
-      "public/screenshots/chinese mustard pickle_01.png",
-      "public/screenshots/chinese mustard pickle_02.png",
-      "public/screenshots/chinese mustard pickle_03.png",
-      "public/screenshots/chinese mustard pickle_04.png",
+      "screenshots/chinese mustard pickle_01.png",
+      "screenshots/chinese mustard pickle_02.png",
+      "screenshots/chinese mustard pickle_03.png",
+      "screenshots/chinese mustard pickle_04.png",
     ],
     content:
       "Delicious Mustard Pickle. (CHINESE RECIPE.) Cover with water one and a half quarts cauliflower sections, one quart diced cucumber, same quantity green tomato chunks, three-quarters of a quart small onions, same quantity chopped green chillies, half a quart diced celery, half a quart sliced carrots. half a cup fine salt, and let stand for 24 hours. Bring to boiling point, and then drain. Blend together f of a cup flour, three-quarters of a tablespoon mustard, two and a half tablespoons powdered tumeric, one and a quarter cups sugar, and mix with one and a half quarts vinegar; bring to boil slowly, and simmer 15 minutes, stirring constantly to prevent burning. Pour over pickles, return to pan and simmer few minutes. Bottle and seal at once.",
@@ -109,8 +146,8 @@ export const recipes = [
   },
   {
     title: "chinese almond wafers",
-    coverImageUrl: "public/illustrations/chinese wafers.png",
-    screenshotUrls: ["public/screenshots/chinese wafers.png"],
+    coverImageUrl: "illustrations/chinese wafers.png",
+    screenshotUrls: ["screenshots/chinese wafers.png"],
     content: `Blanch 1/2 cup almonds, halve, and
     cut in strips, dry in a warm oven. Cream
     1/4 cup butter, 1/4 cup sugar, add 1 egg,
@@ -130,11 +167,11 @@ export const recipes = [
   },
   {
     title: "sorrel soup",
-    coverImageUrl: "public/illustrations/sorrel soup.png",
+    coverImageUrl: "illustrations/sorrel soup.png",
     screenshotUrls: [
-      "public/screenshots/sorrel soup_01.png",
-      "public/screenshots/sorrel soup_02.png",
-      "public/screenshots/sorrel soup_03.png",
+      "screenshots/sorrel soup_01.png",
+      "screenshots/sorrel soup_02.png",
+      "screenshots/sorrel soup_03.png",
     ],
     content:
       "Although at their best in spring, luscious leaves may be had through- out the year. Remove the midribs, cook in water for three minutes, strain, return to the pan, and cream in a tablespoon of rich milk, a nut of butter, teaspoon of flour, pepper and salt. Sorrel soup and sorrel pot- tage, with milk and cream, are cool- ing for summer use. Add nothing to the soup except half a cup of finely chopped watercress, or Chinese mus- tard, just before serving.",
@@ -142,31 +179,6 @@ export const recipes = [
     troveUrl: "https://trove.nla.gov.au/newspaper/article/206345781",
     publisher: "The Age (Melbourne, Vic. : 1854 - 1954)",
     tags: ["fusion", "dish"],
-  },
-
-  {
-    title: "chop suey",
-    coverImageUrl: "public/illustrations/chicken chop suey.png",
-    screenshotUrls: [
-      "public/screenshots/chop suey_01.png",
-      "public/screenshots/chop suey_02.png",
-    ],
-    date: "1 February 1950",
-    content:
-      "Chicken Chop Suey. —Cut cooked chicken or fowl into 1-inch cubes. Dice vegetables similar to those used for sweet-sour dishes. Mushrooms and carrots are frequently used. Put all ingredients in very hot fat and pour over them 1 teaspoon of soy sauce. Lift out vegetables, add a little water to the oil and liquid in the pan, thicken with cornflour and serve with rice. Prawns can replace chicken. The Chinese remove the skin from the prawn in all places bat the tail. When put in hot oil the prawn curls around the unskinned red tail and looks most attractive.  ",
-    troveUrl: "https://nla.gov.au/nla.obj-501748968",
-    publisher: "The Australian woman's mirror",
-    tags: ["dish"],
-  },
-  {
-    title: "chinese chews dessert",
-    coverImageUrl: "public/illustrations/chinese chews dessert.png",
-    screenshotUrls: ["public/screenshots/chinese chews.png"],
-    date: "4 June 1935",
-    content: `One cup sugar, 31b. butter, 1 egg, 1 cup flour, 3 teaspoons cocoa, vanilla to taste, 1 cup chopped dates or walnuts, or both. Cream butter and sugar, add egg and beat again. Then add dry ingredients and fruit. This is a thick enough mixture to spread over greased meat-tin or long sponge-roll tin. Take out of oven before it looks  thoroughly cooked (about 3/4 hour) ; this is important, for if it is well cooked it will be dry instead of chewy. Cut into squares in tin while hot then leave to cool.— \"Ngawini,\” Wanganui, N.Z.  ",`,
-    troveUrl: "https://nla.gov.au/nla.obj-566554307",
-    publisher: "The Australian woman's mirror.",
-    tags: ["dessert"],
   },
 ];
 
