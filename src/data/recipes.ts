91f460c8015d5e8dc05a2bbfc9bb6d4f2626f218
MaggieMeow <maggieliyd@gmail.com>
2023-10-22 19:03:08 +1100

    update images

--- src/data/recipes.ts
+++ src/data/recipes.ts
@@ -2,11 +2,7 @@ export const recipes = [
   {
     title: "dahl",
     coverImageUrl: "public/illustrations/dahl.png",
-    screenshotUrls: [
-      "screenshots/dahl_01.png",
-      "screenshots/dahl_02.png",
-      "screenshots/dahl_03.png",
-    ],
+    screenshotUrls: ["screenshots/dahl.png"],
     content:
       "The celebrated Indian and Chinese dish called dahl has also lentils for its chief ingredient and is purely of vegetable matter, it is substantial and delicials, and is made as follows:-Stew a quart of split lentils till they form a thick soup; have ready a pound of rice, well boiled in milk, and drained off as dry as possible. Shake the rice up loosely in a dish, and, after mixing an ounce of currypowder with the lentils, pour the lentil soup over the rice and serve it up.",
     date: "Saturday 23 July 1870",
@@ -17,7 +13,7 @@ export const recipes = [
   {
     title: "melon jam",
     coverImageUrl: "illustrations/melon jam.png",
-    screenshotUrls: ["screenshots/melon jam.png"],
+    screenshotUrls: ["screenshots/melon-jam.png"],
     content:
       "The quantity is for 20lb., and it is made as follows:- 20lb. of melon cut into large pieces, 20lb. of sugar, the rind and juice of four lemons, 2oz. of green ginger boiled and sliced, and one teaspoonful of cayenne pepper tied tightly in a thick piece of muslin. Boil all together till the melon is clear ; when cooked take out the pepper, add one glass of brandy. N. B -Green ginger can be bought from the Chinese or from Mr. Germain Nicoleon, grocer.",
     date: "Saturday 14 April 1883",
@@ -28,11 +24,7 @@ export const recipes = [
   {
     title: "preserved eggs",
     coverImageUrl: "public/illustrations/preserved eggs.png",
-    screenshotUrls: [
-      "public/screenshots/preserved eggs_01.png",
-      "public/screenshots/preserved eggs_02.png",
-      "public/screenshots/preserved eggs_03.png",
-    ],
+    screenshotUrls: ["public/screenshots/preserved-eggs.png"],
     content:
       "I do not know whether it was from the Chinese we got the recipe for preserving eggs in lime, but it is a fact that they preserve eggs in a solution of wood ashes, lime, and salt, mixed with water in which some aromatic plant has been boiled. This paste is run into a wooden tub, and the eggs taken fresh from the nests are embedded in it in layers. Then the tub is hermetically sealed, and at the end of six weeks the eggs are considered fit for use, but at the end of forty years they arc considered better. For every day use, eggs are steeped in salt water mixed with soot or red clay, in which they arc baked when required. A plain lightly boiled egg is never seen in China, they are always hard boiled, and the older they are the better. In one part of the country the greatest delicacy is unhatched duck roasted. The eggs are taken from the hatching-house twelve hours before hatching, placed in a hot oven and baked, then they are sold from door to door. Strange to say, the blacks of this country also follow this custom. I have known them watch a nest of wild duck eggs for days and days till ready. A correspondent asks the quantity of lime to use for preserving eggs. Take one pint of lime to each gallon of water, stir, and boil fifteen minutes. Let stand till clear and pour off the water, boil up the lime again with half the previous quantity of water, and when clear add to thc first lot. Put the eggs in be- fore they are six hours old. The above is the recipe I have used for years.",
     date: "Tuesday 28 February 1893",
@@ -67,12 +59,7 @@ export const recipes = [
   {
     title: "gingembre a la chantilly",
     coverImageUrl: "illustrations/gingembre a la chantilly.png",
-    screenshotUrls: [
-      "screenshots/Gingembre a la Chantilly_01.png",
-      "screenshots/Gingembre a la Chantilly_02.png",
-      "screenshots/Gingembre a la Chantilly_03.png",
-      "screenshots/Gingembre a la Chantilly_04.png",
-    ],
+    screenshotUrls: ["screenshots/gingembre-a-la-chantilly.png"],
     content:
       "Gingembre a la Chantilly. —Take from 4 to 8 oz. of. Chinese preserved ginger. Cut it quickly into neat squares; Pile those up.: in the centre of a fancy dish. If time permits, whip, up a gill of cream (the contents of one of the sixpenny brown pots gives about this quantity), and pile in on top of the ginger. If pressed for time, pour the cream over it. It does not require sweetening, as the ginger is in itself excessively sweet. Then take a gill of the ginger syrup, add to it tablespoonful of either brandy, rum, or whisky, as preferred. Pour over the cream, and serve.",
     date: "Saturday 10 January 1914",
@@ -109,7 +96,7 @@ export const recipes = [
   {
     title: "dim sims",
     coverImageUrl: "illustrations/dim sims.png",
-    screenshotUrls: ["screenshots/dim sims.png"],
+    screenshotUrls: ["screenshots/dim-sims.png"],
     content:
       'Dim Sims Mr H (Melbourne) supplies this recipe for making dim sims: -Chicken pork or a mixture of both, may be minced or finely shopped to form the base of the filling. Blend with either finely chopped water chestnuts obtainable from a Chinese restaurant and a little "Seeyou" sauce (or Worcester sauce may be used). Add pepper and salt to taste, and fill with the mixture small cases made from leaf macaroni. Ordinary boiling paste may be used if the macaroni is unobtainable Stand in a saucepan containing sufficient boiling water to cover them and simmer gently. Other seasonings may be tested as the art of making this Chinese dish is mastered.',
     date: "Wednesday 15 May 1935",
@@ -120,7 +107,7 @@ export const recipes = [
   {
     title: "chinese chews",
     coverImageUrl: "public/illustrations/chinese chews dessert.png",
-    screenshotUrls: ["public/screenshots/chinese chews.png"],
+    screenshotUrls: ["public/screenshots/chinese-chews.png"],
     date: "4 June 1935",
     content: `One cup sugar, 31b. butter, 1 egg, 1 cup flour, 3 teaspoons cocoa, vanilla to taste, 1 cup chopped dates or walnuts, or both. Cream butter and sugar, add egg and beat again. Then add dry ingredients and fruit. This is a thick enough mixture to spread over greased meat-tin or long sponge-roll tin. Take out of oven before it looks  thoroughly cooked (about 3/4 hour) ; this is important, for if it is well cooked it will be dry instead of chewy. Cut into squares in tin while hot then leave to cool.— \"Ngawini,\” Wanganui, N.Z.  ",`,
     troveUrl: "https://nla.gov.au/nla.obj-566554307",
@@ -131,12 +118,7 @@ export const recipes = [
   {
     title: "mustard pickle",
     coverImageUrl: "illustrations/chinese mustard pickle.png",
-    screenshotUrls: [
-      "screenshots/chinese mustard pickle_01.png",
-      "screenshots/chinese mustard pickle_02.png",
-      "screenshots/chinese mustard pickle_03.png",
-      "screenshots/chinese mustard pickle_04.png",
-    ],
+    screenshotUrls: ["screenshots/chinese-mustard-pickle.png"],
     content:
       "Delicious Mustard Pickle. (CHINESE RECIPE.) Cover with water one and a half quarts cauliflower sections, one quart diced cucumber, same quantity green tomato chunks, three-quarters of a quart small onions, same quantity chopped green chillies, half a quart diced celery, half a quart sliced carrots. half a cup fine salt, and let stand for 24 hours. Bring to boiling point, and then drain. Blend together f of a cup flour, three-quarters of a tablespoon mustard, two and a half tablespoons powdered tumeric, one and a quarter cups sugar, and mix with one and a half quarts vinegar; bring to boil slowly, and simmer 15 minutes, stirring constantly to prevent burning. Pour over pickles, return to pan and simmer few minutes. Bottle and seal at once.",
     date: "Friday 24 November 1939",
@@ -168,11 +150,7 @@ export const recipes = [
   {
     title: "sorrel soup",
     coverImageUrl: "illustrations/sorrel soup.png",
-    screenshotUrls: [
-      "screenshots/sorrel soup_01.png",
-      "screenshots/sorrel soup_02.png",
-      "screenshots/sorrel soup_03.png",
-    ],
+    screenshotUrls: ["screenshots/sorrel-soup.png"],
     content:
       "Although at their best in spring, luscious leaves may be had through- out the year. Remove the midribs, cook in water for three minutes, strain, return to the pan, and cream in a tablespoon of rich milk, a nut of butter, teaspoon of flour, pepper and salt. Sorrel soup and sorrel pot- tage, with milk and cream, are cool- ing for summer use. Add nothing to the soup except half a cup of finely chopped watercress, or Chinese mus- tard, just before serving.",
     date: "Saturday 11 September 1943",
